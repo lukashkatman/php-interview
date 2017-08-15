16f556179f823078dc1a7c6f233a9c1293448734
Ronash Dhakal <ronashmail@gmail.com>
2017-08-15 10:26:53 +0545

    first

--- question/backend.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rnsdk
- * Date: 8/12/2017
- * Time: 12:38 PM
- */
- header("Location: index.php");
--- question/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rnsdk
- * Date: 8/12/2017
- * Time: 12:18 PM
- */
